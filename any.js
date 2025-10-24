be70c072a87746efd76a1ff6209414acdab0a958
PYASantos <b2029pyasantos@pshs.edu.ph>
2025-10-24 21:35:46 +0800

    commit

--- any.js
+++ any.js
@@ -309,4 +309,11 @@ process.stdin.on('data', input => {
 
 });
 
+for (let i = 2; i <= 20; i++){
+    console.log(i);
+}
 
+let letters = ["a", "b", "c"]; 
+letters.push("d");
+letters.shift(); 
+console.log(letters); 
--- practice.js
+++
@@ -1,8 +0,0 @@
-for (let i = 2; i <= 20; i++){
-    console.log(i);
-}
-
-let letters = ["a", "b", "c"]; 
-letters.push("d");
-letters.shift(); 
-console.log(letters); 
